6fb5806fae82b57c502c84eadaf9245d2aa82067
Oleg Kostyuk <cub.uanic@gmail.com>
2012-07-10 17:34:43 +0300

    Bugfixing for Schedules

--- lib/WebService/PagerDuty/Schedules.pm
+++ lib/WebService/PagerDuty/Schedules.pm
@@ -27,9 +27,16 @@ has password => (
 sub entries {
     my ( $self, %params ) = @_;
 
+    my $id = $params{id} || $params{schedule_id} || undef;
+
+    delete $params{id};
+    delete $params{schedule_id};
+
+    die('WebService::PagerDuty::Schedules::entries(): id or schedule_id is required') unless defined $id;
+
     load_class('WebService::PagerDuty::Request');
     return WebService::PagerDuty::Request->new->get(
-        url      => $self->url,
+        url      => URI->new( $self->url . '/' . $id . '/entries' ),
         user     => $self->user,
         password => $self->password,
         params   => \%params,
